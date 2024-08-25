7e170813d8cd69f52869a20016dd6181a85f11b2
rushi <rushikeshsangale57@gmail.com>
2024-08-25 17:54:00 +0530

    Creating Stock class

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,14 @@ public class Main {
         System.out.println("Stock Management Code");
     }
 }
+class Stock{
+    private String name;
+    private int numberOfShares;
+    private double sharePrice;
+
+    public Stock(String name, int numberOfShares, double sharePrice) {
+        this.name = name;
+        this.numberOfShares = numberOfShares;
+        this.sharePrice = sharePrice;
+    }
+}
